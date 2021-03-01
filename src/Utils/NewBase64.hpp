275319e8ef26f58e6ec89d986524254d3bd3aea6
Aleksandr Lyapunov <alyapunov@tarantool.org>
2021-03-01 09:58:25 +0300

    Yet another Base64 codec implementation

---
+++ src/Utils/NewBase64.hpp
@@ -0,0 +1,241 @@
+#pragma once
+/*
+ * Copyright 2010-2020, Tarantool AUTHORS, please see AUTHORS file.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+
+/**
+ * RFC4648 base64 and base64url codecs.
+ * 1. Encoders use alphabets that end with "+/" and "-_" respectively.
+ * 2. Decoders decode both alphabets and even mixed.
+ * 3. Padding (with "=") is added by encoder but required by decoder.
+ * 4. Line feeds are not added by encoder and treated as error by decoder.
+ * 5. Non-alphabet characters are treated as error by decoder.
+ */
+
+#if defined(__GNUC__)
+#if __GNUC__ >= 3
+#define BASE64_COMPLILER_HAS_BUILTIN_EXPECT 1
+#endif
+#endif
+
+#if defined(__has_builtin)
+#if __has_builtin(__builtin_expect)
+#define BASE64_COMPLILER_HAS_BUILTIN_EXPECT 1
+#endif
+#endif
+
+#ifdef BASE64_COMPLILER_HAS_BUILTIN_EXPECT
+#define BASE64_LIKELY(x)   __builtin_expect(!!(x), 1)
+#define BASE64_UNLIKELY(x) __builtin_expect(!!(x), 0)
+#else
+#define BASE64_LIKELY(x)   (x)
+#define BASE64_UNLIKELY(x) (x)
+#endif // #idfef BASE64_COMPLILER_HAS_BUILTIN_EXPECT
+
+namespace base64 {
+
+enum {
+	/** Use "...-_" alphabet instead of "...+/". */
+	URL = 1,
+};
+
+template <class INP, class OUT>
+std::pair<INP, OUT> encode(INP first, INP last, OUT dest, int options = 0)
+{
+	const char *alphabets[2] = {
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+	};
+	const char padding = '=';
+	const char *alphabet = alphabets[(options & URL) ? 1 : 0];
+	assert(alphabet[63] != 0 && alphabet[64] == 0);
+
+	while (true) {
+		if (BASE64_UNLIKELY(first == last))
+			return {first, dest};
+
+		uint32_t part = static_cast<uint8_t>(*first++); // Have 8 bits.
+		*dest++ = alphabet[part >> 2]; // Use high 6 bits.
+		part &= 0x3; // Save low 2 bits.
+
+		if (BASE64_UNLIKELY(first == last)) {
+			*dest++ = alphabet[part << 4]; // Use saved low 2 bits.
+			*dest++ = padding;
+			*dest++ = padding;
+			return {first, dest};
+		}
+
+		part = (part << 8) | static_cast<uint8_t>(*first++); // Have 10.
+		*dest++ = alphabet[part >> 4]; // Use high 6 bits.
+		part &= 0xf; // Save low 4 bits.
+
+		if (BASE64_UNLIKELY(first == last)) {
+			*dest++ = alphabet[part << 2]; // Use saved low 4 bits.
+			*dest++ = padding;
+			return {first, dest};
+		}
+
+		part = (part << 8) | static_cast<uint8_t>(*first++); // Have 12.
+		*dest++ = alphabet[part >> 6]; // Use high 6 bits.
+		*dest++ = alphabet[part & 0x3f]; // Use low 6 bits.
+	}
+}
+
+template <class INP, class OUT>
+std::pair<INP, OUT> decode(INP first, INP last, OUT dest)
+{
+	// Map to decode back from alphabet to 6bit integer (0..63).
+	// Has -1 for wrong characters and 64 for padding ('=') character.
+	const char *decmap =
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\076\377\076\377\077"
+		"\064\065\066\067\070\071\072\073\074\075\377\377\377\100\377\377"
+		"\377\000\001\002\003\004\005\006\007\010\011\012\013\014\015\016"
+		"\017\020\021\022\023\024\025\026\027\030\031\377\377\377\377\077"
+		"\377\032\033\034\035\036\037\040\041\042\043\044\045\046\047\050"
+		"\051\052\053\054\055\056\057\060\061\062\063\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377"
+		"\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377\377";
+	
+	while (true) {
+		if (BASE64_UNLIKELY(first == last))
+			return {first, dest};
+
+		uint8_t c = decmap[static_cast<uint8_t>(*first)];
+		if (BASE64_UNLIKELY(c >= 64)) // Bad char or '=' here is error.
+			return {first, dest};
+		uint32_t part = c; // 6 bits.
+
+		auto next = first;
+		++next;
+		if (BASE64_UNLIKELY(next == last)) // Unexpected end.
+			return {first, dest};
+
+		c = decmap[static_cast<uint8_t>(*next)];
+		if (BASE64_UNLIKELY(c >= 64)) // Bad char or '=' here is error.
+			return {first, dest};
+
+		part = (part << 6) | c; // 12 useful bits.
+		*dest++ = static_cast<char>(part >> 4); // 1st char is accepted.
+		// Now `part` has 4 lower useful bits and garbage in upper bits.
+		first = next;
+		++next;
+
+		if (BASE64_UNLIKELY(next == last)) {
+			// Now end of stream is allowed.
+			if (BASE64_UNLIKELY((part & 0xf) != 0))
+				// Incorrect (non-zero) remainder.
+				return {first, dest};
+
+			return {next, dest};
+		}
+
+		c = decmap[static_cast<uint8_t>(*next)];
+		if (BASE64_UNLIKELY(c >= 64)) {
+			if (BASE64_UNLIKELY((part & 0xf) != 0))
+				// Incorrect (non-zero) remainder.
+				return {first, dest};
+
+			if (BASE64_UNLIKELY(c > 64)) // Wrong character, leave.
+				return {next, dest};
+
+			++next; // '=' character, take it.
+			// If the next character is '=' we should also take it.
+			if (BASE64_LIKELY(next != last && *next == '='))
+				++next;
+
+			return {next, dest};
+		}
+
+		part = (part << 6) | c; // 10 usefult bits (and garbage above).
+		*dest++ = static_cast<char>(part >> 2); // 2nd char is accepted.
+		// Now `part` has 2 lower useful bits and garbage in upper bits.
+		first = next;
+		++next;
+
+		if (BASE64_UNLIKELY(next == last)) {
+			// Now end of stream is allowed.
+			if (BASE64_UNLIKELY((part & 0x3) != 0))
+				// Incorrect (non-zero) remainder.
+				return {first, dest};
+
+			return {next, dest};
+		}
+
+		c = decmap[static_cast<uint8_t>(*next)];
+		if (BASE64_UNLIKELY(c >= 64)) {
+			if (BASE64_UNLIKELY((part & 0x3) != 0))
+				// Incorrect (non-zero) remainder.
+				return {first, dest};
+
+			if (BASE64_UNLIKELY(c > 64)) // Wrong character, leave.
+				return {next, dest};
+
+			++next; // '=' character, take it.
+			return {next, dest};
+		}
+
+		part = (part << 6) | c; // 8 usefult bits (and garbage above).
+		*dest++ = static_cast<char>(part); // 3rd char is accepted.
+		first = next;
+		++first;
+	}
+}
+
+/**
+ * Calculate exact buffer size required for encoding @a src_size bytes of data.
+ */
+inline size_t enc_size(size_t src_size)
+{
+	return (src_size + 2) / 3 * 4;
+}
+
+/**
+ * Calculate buffer size required for decoding @a src_size bytes of data.
+ * Actual size can be less by 1 or 2 because of base64 padding.
+ */
+inline size_t dec_size(size_t src_size)
+{
+	return src_size * 3 / 4;
+}
+
+} // namespace base64 {
---
+++ test/Base64UnitTest.cpp
@@ -0,0 +1,243 @@
+/*
+ * Copyright 2010-2020, Tarantool AUTHORS, please see AUTHORS file.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "../src/Utils/NewBase64.hpp"
+
+#include <cassert>
+#include <cstring>
+
+#include "Utils/Helpers.hpp"
+
+// Proof that the codec's constants are correct.
+static void
+gen_src_strings()
+{
+	char alphabets[2][65];
+	for (size_t i = 0; i < 2; i++) {
+		char *alphabet = alphabets[i];
+		for (char c = 'A'; c <= 'Z'; c++)
+			*alphabet++ = c;
+		for (char c = 'a'; c <= 'z'; c++)
+			*alphabet++ = c;
+		for (char c = '0'; c <= '9'; c++)
+			*alphabet++ = c;
+		if (i == 0) {
+			*alphabet++ = '+';
+			*alphabet++ = '/';
+		} else {
+			*alphabet++ = '-';
+			*alphabet++ = '_';
+		}
+		*alphabet = 0;
+		fail_if(alphabet - alphabets[i] != 64);
+	}
+
+	char decmap[256];
+	for (size_t i = 0; i < 256; i++)
+		decmap[i] = -1;
+	for (size_t i = 0; i < 2; i++)
+		for (size_t j = 0; j < 64; j++)
+			decmap[static_cast<uint8_t>(alphabets[i][j])] = j;
+	decmap['='] = 64;
+
+	std::cout << "\tconst char *alphabets[2] = {\n";
+	for (size_t i = 0; i < 2; i++)
+		std::cout << "\t\t\"" << alphabets[i] << (i ? "\"\n" : "\",\n");
+	std::cout << "\t};\n";
+
+	std::cout << "\tconst char *decmap =\n";
+	for (size_t i = 0; i < 16; i++) {
+		std::cout << "\t\t\"";
+		for (size_t j = 0; j < 16; j++) {
+			uint8_t c = decmap[i * 16 + j];
+			std::cout << "\\"
+				  << static_cast<char>(('0' + (c >> 6)))
+				  << static_cast<char>(('0' + ((c >> 3) & 0x7)))
+				  << static_cast<char>(('0' + (c & 0x7)));
+		}
+		std::cout << (i < 15 ? "\"\n" : "\";\n");
+	}
+}
+
+// Encode `orig` an compare with `encoded`. Decode back and compare with `orig`.
+static void
+simple_test(const char *orig, size_t orig_size,
+	    const char *encoded, size_t encoded_size)
+{
+	char buf[256];
+	fail_if(encoded_size > sizeof(buf));
+
+	{
+		auto [inp, out] = base64::encode(orig, orig + orig_size, buf);
+		size_t processed_src = inp - orig;
+		size_t processed_dst = out - buf;
+		fail_unless(processed_src == orig_size);
+		fail_unless(processed_dst == base64::enc_size(orig_size));
+		fail_unless(processed_dst == encoded_size);
+		fail_unless(memcmp(buf, encoded, processed_dst) == 0);
+	}
+
+	{
+		auto [inp, out] = base64::decode(encoded, encoded + encoded_size, buf);
+		size_t processed_src = inp - encoded;
+		size_t processed_dst = out - buf;
+		fail_unless(processed_src == encoded_size);
+		fail_unless(processed_dst == base64::dec_size(encoded_size) ||
+			    processed_dst + 1 == base64::dec_size(encoded_size) ||
+			    processed_dst + 2 == base64::dec_size(encoded_size));
+		fail_unless(processed_dst == orig_size);
+		fail_unless(memcmp(buf, orig, processed_dst) == 0);
+	}
+
+}
+
+// Several tests that was generated with standard base64 utility.
+static void
+simple_tests()
+{
+	simple_test("", 0, "", 0);
+	simple_test("a", 1, "YQ==", 4);
+	simple_test("aa", 2, "YWE=", 4);
+	simple_test("aaa", 3, "YWFh", 4);
+	simple_test("aaaaa", 4, "YWFhYQ==", 8);
+}
+
+static void
+forth_and_back_test(const char *orig, size_t orig_size)
+{
+	char buf[256];
+	fail_if(base64::enc_size(orig_size) > sizeof(buf));
+
+	size_t encoded_size;
+	{
+		auto [inp, out] = base64::encode(orig, orig + orig_size, buf);
+		size_t processed_src = inp - orig;
+		encoded_size = out - buf;
+		fail_unless(processed_src == orig_size);
+		fail_unless(encoded_size == base64::enc_size(orig_size));
+	}
+
+	char buf2[256];
+	size_t decoded_size;
+	{
+		auto [inp, out] = base64::decode(buf, buf + encoded_size, buf2);
+		size_t processed_src = inp - buf;
+		decoded_size = out - buf2;
+		fail_unless(processed_src == encoded_size);
+		fail_unless(decoded_size == base64::dec_size(encoded_size) ||
+			    decoded_size + 1 == base64::dec_size(encoded_size) ||
+			    decoded_size + 2 == base64::dec_size(encoded_size));
+		fail_unless(decoded_size == orig_size);
+		fail_unless(memcmp(buf2, orig, orig_size) == 0);
+	}
+}
+
+static void
+forth_and_back_tests()
+{
+	constexpr size_t K = 2;
+	constexpr size_t N[K] = {1024 * 1024, 128 * 1024}; // Number of runs.
+	constexpr size_t M[K] = {4, 128}; // Limit of string's length.
+	char buf[M[K - 1]];
+
+	for (size_t k = 0; k < K; k++) {
+		for (size_t i = 0; i < N[k]; i++) {
+			size_t s = 1 + rand() % (M[k] - 1);
+			for (size_t j = 0; j < s; j++)
+				buf[j] = rand();
+			forth_and_back_test(buf, s);
+		}
+	}
+}
+
+static void
+check_bad_ending(const char *enc,
+		 size_t expected_consumed, size_t expected_produced,
+		 const char *expected_dec)
+{
+	size_t enc_size = strlen(enc);
+	char res[256];
+	auto [cons_end,prod_end] = base64::decode(enc, enc + enc_size, res);
+	size_t real_cons = cons_end - enc;
+	size_t real_prod = prod_end - res;
+	fail_unless(expected_consumed == real_cons);
+	fail_unless(expected_produced == real_prod);
+	fail_unless(memcmp(res, expected_dec, expected_produced) == 0);
+}
+
+static void
+check_bad_endings()
+{
+	check_bad_ending("", 0, 0, "");
+
+	for (size_t len = 1; len <= 4; len++) {
+		char data[8];
+		data[len] = 0;
+		for (uint32_t mask = 0; mask < 1u << len; mask++) {
+			for (size_t i = 0; i < len; i++)
+				data[i] = (mask & (1u << i)) ? '*' : '=';
+			check_bad_ending(data, 0, 0, "");
+		}
+	}
+
+	check_bad_ending("/", 0, 0, "");
+	check_bad_ending("/*", 0, 0, "");
+	check_bad_ending("/=", 0, 0, "");
+
+	check_bad_ending("/w==", 4, 1, "\xff");
+	check_bad_ending("/w==*", 4, 1, "\xff");
+	check_bad_ending("/w=", 3, 1, "\xff");
+	check_bad_ending("/w=*", 3, 1, "\xff");
+	check_bad_ending("/w", 2, 1, "\xff");
+	check_bad_ending("/w*", 2, 1, "\xff");
+
+	check_bad_ending("/x", 1, 1, "\xff");
+	check_bad_ending("/x==", 1, 1, "\xff");
+	check_bad_ending("/x*", 1, 1, "\xff");
+
+	check_bad_ending("//8=", 4, 2, "\xff\xff");
+	check_bad_ending("//8=*", 4, 2, "\xff\xff");
+	check_bad_ending("//8", 3, 2, "\xff\xff");
+	check_bad_ending("//8*", 3, 2, "\xff\xff");
+
+	check_bad_ending("//9", 2, 2, "\xff\xff");
+	check_bad_ending("//9=", 2, 2, "\xff\xff");
+	check_bad_ending("//9*", 2, 2, "\xff\xff");
+
+}
+
+int main()
+{
+	gen_src_strings();
+	simple_tests();
+	forth_and_back_tests();
+	check_bad_endings();
+}
