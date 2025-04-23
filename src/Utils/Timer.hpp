7e69aaba69f7af4a4f9b2372fe734e048c0ad31b
Andrey Saranchin <Andrey22102001@gmail.com>
2025-04-23 12:45:51 +0300

    client: handle timeout more safely
    Currently all wait methods are just cycling in a loop while timer is not
    expired. At the beginning of each iteration, `timeout - timer.elapsed()`
    expression is used to calculate timeout for `NetProvider::wait()`, and
    that `wait` expects only non-negative timeouts (or `-1` for infinity).
    So, for example, if the operating system will reschedule the process
    right between `timer.isExpired()` and `timer.elapsed()` call, timer
    can become expired after we are scheduled again, and we will suddenly
    pass a negative value to the provider.
    
    Let's replace `elapsed` method with more safe `timeLeft` which returns
    zero when the timeout is expired. Also, such method will be handy in the
    following commits.
    
    Closes #128

--- src/Utils/Timer.hpp
+++ src/Utils/Timer.hpp
@@ -29,6 +29,7 @@
  * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+#include <algorithm>
 #include <chrono>
 
 class Timer {
@@ -48,13 +49,22 @@ public:
 			std::chrono::duration_cast<std::chrono::milliseconds>(end - m_Start);
 		return elapsed >= m_Timeout;
 	}
-	int elapsed() const
+	/**
+	 * The function to obtain amount of time left. Returns:
+	 * 1. `-1` if the initial timeout was `-1`.
+	 * 2. `0` if the timer has expired.
+	 * 3. Otherwise, amount of milliseconds left is returned.
+	 * NB: the function should not be used for expiration check - use `isExpired` instead.
+	 */
+	int timeLeft() const
 	{
 		if (m_Timeout == std::chrono::milliseconds{-1})
-			return 0;
+			return -1;
 		std::chrono::time_point<std::chrono::steady_clock> end =
 			std::chrono::steady_clock::now();
-		return std::chrono::duration_cast<std::chrono::milliseconds>(end - m_Start).count();
+		int timeLeft = m_Timeout.count() -
+			std::chrono::duration_cast<std::chrono::milliseconds>(end - m_Start).count();
+		return std::max(0, timeLeft);
 	}
 private:
 	std::chrono::milliseconds m_Timeout;
