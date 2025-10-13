efe4da265f019d35720d82c3f393ce48fa87cf1c
Georgiy Lebedev <georgiy.lebedev@epfl.ch>
2025-10-13 20:46:07 +0200

    client: fix `connect` error handling
    Currently, if we timeout waiting for `connect` using `poll`, we set
    `connect_errno` to `ETIMEOUT`. However, after we exit the `connect`
    interruption loop, we unconditionally set to `errno`, which we do not even
    clear after checking the `connect` result. As a result, the timeout error
    gets masked by the in-progress connection error.
    
    To fix this, let's:
    1. Clear `errno` after checking the `connect` result;
    2. Check the value of `connect_errno` after exiting the `connect` interrupt
       loop before setting it to `errno`;
    3. Zero out `socket_errno`, `connect_errno` each `addr_info` loop
       iteration.
    
    Closes #151

--- src/Client/UnixStream.hpp
+++ src/Client/UnixStream.hpp
@@ -169,6 +169,7 @@ UnixStream::connect(const ConnectOptions &opts_arg)
 			      addr_info.last_error());
 	int socket_errno = 0, connect_errno = 0;
 	for (auto &inf: addr_info) {
+		socket_errno = connect_errno = 0;
 		fd = ::socket(inf.ai_family, inf.ai_socktype, inf.ai_protocol);
 		if (fd < 0) {
 			socket_errno = errno;
@@ -190,10 +191,7 @@ UnixStream::connect(const ConnectOptions &opts_arg)
 				return US_TELL(SS_ESTABLISHED,
 					       "Connected", opts);
 			} else if (errno == EINPROGRESS || errno == EAGAIN) {
-				// TODO remove timeout and #include <poll.h>
-				//return US_TELL(SS_CONNECT_PENDING,
-				//	       "Connect pending", opts);
-
+				errno = 0;
 				set_status(SS_CONNECT_PENDING);
 				struct pollfd fds;
 				fds.fd = fd;
@@ -207,7 +205,10 @@ UnixStream::connect(const ConnectOptions &opts_arg)
 			}
 		} while (errno == EINTR);
 		close();
-		connect_errno = errno;
+		if (connect_errno == 0)
+			connect_errno = errno;
+		else
+			assert(connect_errno == ETIMEDOUT && errno == 0);
 	}
 	if (connect_errno != 0)
 		return US_DIE("Failed to connect", strerror(connect_errno));
